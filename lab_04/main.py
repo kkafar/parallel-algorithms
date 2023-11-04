8544d15488ecb9549cf3deff861f8566c13a25a0
Kacper Kafara <kacperkafara@gmail.com>
2023-11-04 18:16:19 +0100

    First version of scripts for lab4

---
+++ lab_04/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import numpy as np
+import argparse
+from mpi4py import MPI
+from timeit import default_timer as timer
+from dataclasses import dataclass
+
+
+def build_cli() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(prog="solver")
+    parser.add_argument('-s', '--series', type=int, required=True, dest='series', help='Id of the series. THIS DOES NOT MEAN COMPUTATION WILL BE REPEATED')
+    parser.add_argument('--points-per-proc', type=int, required=True, dest='ppc', help='Resolution for each process')
+    parser.add_argument('-a', '--side', type=float, required=True, dest='a', help='Side length of the membrane')
+    parser.add_argument('--theta', type=float, required=True, dest='theta', help='Right side of the equation')
+    parser.add_argument('--iters', type=int, required=True, dest='iters', help='Number of iterations in iterative method')
+    return parser
+
+
+@dataclass
+class Args:
+    series: int
+    ppc: int
+    a: float
+    theta: float
+    iters: int
+
+
+def compute(comm, rank, size, delta, ppc, theta, iters):
+    """
+    :param delta: resolution of the grid
+    """
+
+    side = rank * ppc
+    # Rectangle this process is responsible for
+    H = np.zeros((ppc, side))
+
+    x_min = 0 if rank > 0 else 1
+    x_max = ppc if rank < size - 1 else ppc - 1
+
+    for i in range(iters):
+        H_i = np.zeros((ppc, side), dtype=np.float64)
+        if i > 0:
+            # We receive values from last iteration from our neighs
+            recv_buff = np.empty(side, dtype=np.float64)
+            if rank > 0:
+                comm.Recv(recv_buff, rank - 1, i - 1)
+                H_i[0] += recv_buff
+            if rank < size - 1:
+                comm.Recv(recv_buff, rank + 1, i - 1)
+                H_i[-1] += recv_buff
+
+        # We apply the formula
+        H_i[x_min: x_max, 1: side - 1] -= delta ** 2 * theta
+        H_i[x_min:x_max, 1: side - 1] += H[x_min:x_max, 0: side - 2]
+        H_i[x_min:x_max, 1: side - 1] += H[x_min:x_max, 2:side]
+        H_i[1:x_max, :] += H[0: x_max - 1, :]
+        H_i[x_min: ppc - 1, :] += H[x_min + 1: ppc, :]
+
+        H_i /= 4
+        H = H_i
+
+        if rank > 0:
+            comm.Isend(H[0].copy(), rank - 1, i)
+
+        if rank < size - 1:
+            comm.Isend(H[-1].copy(), rank + 1, i)
+
+    return H
+
+
+def main():
+    args: Args = build_cli().parse_args()
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    # Hey, I'm not counting in argument parsing as sequential part of the program,
+    # as it could be completely avoided and is done only for convenience
+    start_time = timer()
+    delta = args.a / (args.ppc * size - 1)
+    stride = compute(comm, rank, size, delta, args.ppc, args.theta, args.iters)
+    recv_buff = comm.gather(stride, root=0)
+    if rank == 0:
+        recv_buff = np.concatenate(recv_buff, axis=0)
+        elapsed = timer() - start_time  # Result is in seconds, we want to convert it to milis
+        # "process_count,problem_size,series_id,time"
+        print(f'{size},{args.ppc},{args.series},{elapsed * 1000}')
+
+
+if __name__ == "__main__":
+    main()
---
+++ lab_04/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+
+#SBATCH --account=plgar2023-cpu
+#SBATCH --time=00:01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=plgrid
+#SBATCH --cpus-per-task=36
+#SBATCH --mem-per-cpu=512M
+
+module purge
+module load scipy-bundle/2021.10-intel-2021b
+
+# I do not think we want the tasks to share resources
+# export SLURM_OVERLAP=1
+
+problem_sizes=(256 512 1024)
+side_length=256
+cpu_min_count=1
+cpu_max_count=36
+series_count=5
+theta=128
+iters=64
+csv_header="process_count,problem_size,series_id,time"
+output_dir="${SCRATCH}/ar"
+output_file="${output_dir}/ar/$(date +%Y-%m-%dT%H:%M:%S)"
+
+mkdir -p $output_dir
+echo $csv_header > $output_file
+
+for (( n_cpu = $cpu_min_count ; n_cpu <= $cpu_max_count ; n_cpu++ )); do
+  for problem_size in "${problem_sizes[@]}"; do
+    for (( series_id = 0 ; series_id < $series_count ; series_id++ )); do
+      echo "Would run: mpiexec -np $n_cpu ./main.py --series $series_id --side $side_length --theta $theta --iters 64 --points-per-proc $problem_size >> $output_file"
+      # mpiexec -np $n_cpu ./main.py --series $series_id --side $side_length --theta $theta --iters 64 --points-per-proc $problem_size >> $output_file
+    done
+  done
+done
+
+# zip -q "${output_file}.zip" $output_file
+# rm -f $output_file
+
+# mpiexec ./main.py 10 13 255 30
+
