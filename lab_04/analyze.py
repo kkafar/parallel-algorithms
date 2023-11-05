bcbf63d4fe44e87c466693605c01f2b5e3bf8a25
Kacper Kafara <kacperkafara@gmail.com>
2023-11-05 21:57:59 +0100

    Add solution for lab 4 (#4)
    * First version of scripts for lab4
    
    * Fix bug in namign output dir
    
    * Modify datetime format
    
    * Check completion
    
    * Bash does not floating point arithmetic
    
    * Uncomment mpiexec
    
    * File naming
    
    * Try to fix issues with message truncation
    
    * Try out sequential communication
    
    * Log some shit
    
    * FIND THE BUG
    
    * Add data processing script
    
    * Add timestamps to loggin in batch script
    
    * Run up to 24 cpus
    
    * Set timelimit to 4h
    
    * Setup C++ project
    
    * Rename c -> cxx
    
    * Attempt to resolve the issues
    
    * Try to use Gather for buffers
    
    * Allocate more cpus
    
    * Try to update runscript
    
    * Another fix
    
    * Try bigger problem sizes
    
    * Measure time of communication & computation
    
    * Update problem sizes
    
    * Hopefully fix the bug
    
    * Fix bug in printing
    
    * Another fix
    
    * Reduce requested resources

---
+++ lab_04/analyze.py
@@ -0,0 +1,154 @@
+import matplotlib.pyplot as plt
+import argparse
+from pathlib import Path
+from dataclasses import dataclass
+from polars import DataFrame, col, Config, read_csv
+
+COL_PROCESS_COUNT = 'process_count'
+COL_PROBLEM_SIZE = 'problem_size'
+COL_SERIES_ID = 'series_id'
+COL_TIME = 'time'
+COL_TIME_AVG = COL_TIME + '_avg'
+COL_TIME_STD = COL_TIME + '_std'
+COL_SPEEDUP = 'speedup'
+COL_SPEEDUP_AVG = COL_SPEEDUP + '_avg'
+COL_SPEEDUP_STD = COL_SPEEDUP + '_std'
+COL_EFFECTIVENES = 'effectivenes'
+COL_EFFECTIVENES_AVG = COL_EFFECTIVENES + '_avg'
+COL_EFFECTIVENES_STD = COL_EFFECTIVENES + '_std'
+COL_KF = 'karp_flatt'
+COL_KF_AVG = COL_KF + '_avg'
+COL_KF_STD = COL_KF + '_std'
+COL_COMPUTE_TIME = 'compute_time'
+COL_GATHER_TIME = 'gather_time'
+
+
+@dataclass
+class Args:
+    input_file: Path
+
+
+def build_cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-file', type=Path, required=True, dest='input_file', help='Path to input file')
+    return parser
+
+
+def get_single_cpu_time(df: DataFrame) -> DataFrame:
+    problem_sizes = (
+        df.lazy()
+        .filter((col(COL_PROCESS_COUNT) == 1))
+        .group_by(col(COL_PROBLEM_SIZE))
+        .agg([
+            col(COL_TIME).mean().alias(COL_TIME_AVG)
+        ])
+        .sort(COL_PROBLEM_SIZE)
+        .collect()
+    )
+    return problem_sizes
+
+
+def main():
+    args: Args = build_cli().parse_args()
+    Config.set_tbl_rows(100)
+    Config.set_tbl_cols(20)
+    plt.rcParams['figure.figsize'] = (16, 9)
+    plt.rcParams["errorbar.capsize"] = 2
+
+    # process_count,problem_size,series_id,time
+    df_main = read_csv(args.input_file, has_header=True).sort([col(COL_PROCESS_COUNT), col(COL_PROBLEM_SIZE)])
+    df_single_cpu_time = get_single_cpu_time(df_main)
+    problem_sizes = df_single_cpu_time.get_column(COL_PROBLEM_SIZE)
+    single_cpu_times = df_single_cpu_time.get_column(COL_TIME_AVG)
+
+    fig, axes = plt.subplots(nrows=2, ncols=2)
+    common_plot_args = {'capthick': 1.4, 'linestyle': ''}
+    markers = ['.', 'x', '^']
+    assert len(markers) == len(problem_sizes)
+
+    for i, problem_size in enumerate(problem_sizes):
+        df_per_size = df_main.filter(col(COL_PROBLEM_SIZE) == problem_size)
+        df_per_size = (
+            df_per_size.lazy()
+            .with_columns([
+                (single_cpu_times.item(i) / col(COL_TIME)).alias(COL_SPEEDUP)
+            ])
+            .with_columns([
+                (col(COL_SPEEDUP) / col(COL_PROCESS_COUNT)).alias(COL_EFFECTIVENES),
+                (((1 / col(COL_SPEEDUP)) - (1 / col(COL_PROCESS_COUNT))) / (1 - (1 / col(COL_PROCESS_COUNT)))).alias(COL_KF)
+            ])
+            .group_by([col(COL_PROCESS_COUNT), col(COL_PROBLEM_SIZE)])
+            .agg([
+                col(COL_TIME).mean().alias(COL_TIME_AVG),
+                col(COL_TIME).std().alias(COL_TIME_STD),
+                col(COL_SPEEDUP).mean().alias(COL_SPEEDUP_AVG),
+                col(COL_SPEEDUP).std().alias(COL_SPEEDUP_STD),
+                col(COL_EFFECTIVENES).mean().alias(COL_EFFECTIVENES_AVG),
+                col(COL_EFFECTIVENES).std().alias(COL_EFFECTIVENES_STD),
+                col(COL_KF).mean().alias(COL_KF_AVG),
+                col(COL_KF).std().alias(COL_KF_STD)
+            ])
+            .sort(col(COL_PROCESS_COUNT))
+            .collect()
+        )
+
+        ax: plt.Axes = axes[0][0]
+        x_data = df_per_size.get_column(COL_PROCESS_COUNT)
+        y_data = df_per_size.get_column(COL_TIME_AVG)
+        y_err_data = df_per_size.get_column(COL_TIME_STD)
+
+        ax.errorbar(x_data, y_data, yerr=y_err_data, label=f'Rozmiar: {problem_size}', marker=markers[i], **common_plot_args)
+        # ax.plot(x_data, [single_cpu_times.item(i) / x for x in x_data], linestyle='--')
+        ax.set(
+            title='Czas wykonania w zależności od liczby procesorów',
+            xlabel='Liczba procesorów',
+            ylabel='Czas wykonania [ms]'
+        )
+        ax.grid()
+        ax.legend()
+
+        ax = axes[0][1]
+        y_data = df_per_size[COL_SPEEDUP_AVG]
+        y_err_data = df_per_size[COL_SPEEDUP_STD]
+        ax.errorbar(x_data, y_data, yerr=y_err_data, label=f'Rozmiar: {problem_size}', marker=markers[i], **common_plot_args)
+        ax.set(
+            title='Przyśpieszenie',
+            xlabel='Liczba procesorów',
+            ylabel='Przyśpieszenie'
+        )
+        ax.grid()
+        ax.legend()
+
+        ax = axes[1][0]
+        y_data = df_per_size[COL_EFFECTIVENES_AVG]
+        y_err_data = df_per_size[COL_EFFECTIVENES_STD]
+        ax.errorbar(x_data, y_data, yerr=y_err_data, label=f'Rozmiar: {problem_size}', marker=markers[i], **common_plot_args)
+        ax.set(
+            title='Efektywność',
+            xlabel='Liczba procesorów',
+            ylabel='Efektywność'
+        )
+        ax.grid()
+        ax.legend()
+
+        ax = axes[1][1]
+        y_data = df_per_size[COL_KF_AVG]
+        y_err_data = df_per_size[COL_KF_STD]
+        ax.errorbar(x_data, y_data, yerr=y_err_data, label=f'Rozmiar: {problem_size}', marker=markers[i], **common_plot_args)
+        ax.set(
+            title='Metryka Karpa-Flatta',
+            xlabel='Liczba procesorów',
+            ylabel='Wartość metryki'
+        )
+        ax.grid()
+        ax.legend()
+
+        print(df_per_size)
+
+    fig.tight_layout()
+    fig.savefig(args.input_file.parent.joinpath(args.input_file.stem).with_suffix('.png'))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ lab_04/run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -l
+#SBATCH --account=plgar2023-cpu
+#SBATCH --time=02:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=plgrid
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=512M
+
+module purge
+module load scipy-bundle/2021.10-intel-2021b
+# module add .plgrid plgrid/tools/openmpi
+
+# I do not think we want the tasks to share resources
+# export SLURM_OVERLAP=1
+
+problem_sizes=(1024 2048 4096)
+side_length=8192
+cpu_min_count=1
+cpu_max_count=16
+series_count=5
+theta=256
+iters=64
+csv_header="process_count,problem_size,series_id,time"
+output_dir="${SCRATCH}/ar"
+output_file_base="${output_dir}/$(date +%Y%m%dT%H%M%S)"
+output_file="${output_dir}/$(date +%Y%m%dT%H%M%S).csv"
+
+mkdir -p $output_dir
+echo $csv_header > $output_file
+
+total_task=$(( "${#problem_sizes[@]}" * (cpu_max_count - cpu_min_count + 1) * series_count ))
+completed_task=0
+
+for (( n_cpu = $cpu_min_count ; n_cpu <= $cpu_max_count ; n_cpu++ )); do
+  for problem_size in "${problem_sizes[@]}"; do
+    for (( series_id = 0 ; series_id < $series_count ; series_id++ )); do
+      echo "[$(date +%Y%m%dT%H%M%S)] Run: mpiexec -np $n_cpu ./main.py --series $series_id --side $side_length --theta $theta --iters 64 --grid-points $problem_size >> $output_file"
+      mpiexec -np $n_cpu ./main.py --series $series_id --side $side_length --theta $theta --iters 64 --grid-points $problem_size >> $output_file
+      completed_task=$(( $completed_task + 1 ))
+      echo "[$(date +%Y%m%dT%H%M%S)] Completion: $completed_task / $total_task"
+    done
+  done
+done
+
+zip -q "${output_file_base}.zip" $output_file
+# rm -f $output_file
+
+# mpiexec ./main.py 10 13 255 30
+
